9a13afb2816cac0259a654d9932406bfa941f693
Guillaume Ferber <guillaume.ferber@web-atrio.com>
2016-10-24 13:55:00 +0200

    Fix stat object & display bmi/weight in table

--- core/modules/measures/measures_weight.php
+++ core/modules/measures/measures_weight.php
@@ -18,7 +18,7 @@ $start_values = [];
 $res = $mysqli->query("SELECT weight, date, height, start_weight, target_weight, neck, chest, shoulders, arm_right, arm_left, calf_right, calf_left, thigh_right, thigh_left, waist FROM body_measures, user WHERE id_user = '$id' AND body_measures.id_user = user.id");
 if(mysqli_num_rows($res) === 0){
     die("0");
-}elseif (mysqli_num_rows($res) > 1) { // more than 1 row
+}elseif (mysqli_num_rows($res) > 0) { // more than 1 row
     while ($arr = mysqli_fetch_assoc($res)) {
         // duplicate values for late exploit
         $start_values = $arr;
@@ -35,8 +35,8 @@ if(mysqli_num_rows($res) === 0){
         $tmp_arr['thigh_left'] = intval($arr['thigh_left']);
         $tmp_arr['waist'] = intval($arr['waist']);
         $tmp_arr2[intval($arr['date'])][] = $tmp_arr;
-    }
 
+    }
     // sort array by date
     ksort($tmp_arr2);
 
@@ -45,15 +45,17 @@ if(mysqli_num_rows($res) === 0){
         $date_arr[] = ts2date($key);
         $val_arr[] = $value[0];
     }
-} elseif (mysqli_num_rows($res) == 1) { // 1 row
+} /*elseif (mysqli_num_rows($res) == 1) { // 1 row
     $arr = mysqli_fetch_assoc($res);
     $start_values = $arr;
     $date_arr[] = ts2date(intval($arr["date"]));
     $val_arr[] = intval($arr['weight']);
-}
+
+}*/
 // join arrays into one array
 $a[] = $date_arr;
 $a[] = $val_arr;
+
 // push values at the beginning of the main array = non dynamic user infos
 array_unshift($a, [intval($start_values['height']), intval($start_values['start_weight']), intval($start_values['target_weight'])]);
 mysqli_free_result($res);
